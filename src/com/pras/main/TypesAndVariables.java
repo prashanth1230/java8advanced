76eb1db6b547c6aa9b99995d5290c70745005513
prashanth1230 <prashanth1230@gmail.com>
2016-01-03 13:18:44 +0530

    Completed the first excersice with data types,

--- src/com/pras/main/TypesAndVariables.java
+++ src/com/pras/main/TypesAndVariables.java
@@ -11,6 +11,16 @@ public class TypesAndVariables {
 
         byte age = 32;
         short numberShipsEncountered = 358;
+        int numberOfPlanetsDiscovered = 126893;
+        long distanceTravelled = 1234455533221L;
+        float numberOfHours = 43800.0f;
+        double costOfMission = 233534534533.34;
+        boolean missionSuccess = true;
 
+        String output = String.format("Example text %s. Using characters also %c. Some numbers %d, %d. Some float " +
+                        "numbers %.2f and %.1f. Finally boolean - %b",
+                fName, middleInit, age, numberShipsEncountered, numberOfHours, costOfMission, missionSuccess);
+
+        System.out.println(output);
     }
 }

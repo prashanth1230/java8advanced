9b40cc1350ea697b913a6b72657592a907b6fbff
Prashanth Manjunath <prashanth1230@gmail.com>
2016-01-03 12:55:45 +0530

    First Commit.

---
+++ src/com/company/TypesAndVariables.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class TypesAndVariables {
+
+    public static void main(String[] args) {
+        //Variables
+        String fName = "James";
+        char middleInit = 'T';
+        char middleInitSymbol = '.';
+        String lName = "Kirk";
+
+        byte age = 32;
+        short numberShipsEncountered = 358;
+
+    }
+}
